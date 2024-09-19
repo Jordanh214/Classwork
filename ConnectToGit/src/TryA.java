275563fd834066b8be3559bb6f949be445af2428
leeca <leeca@10.228.12.124>
2024-09-19 10:51:04 -0400

    Test

--- ConnectToGit/src/TryA.java
+++ ConnectToGit/src/TryA.java
@@ -4,6 +4,7 @@ public class TryA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("TryA message");
+		System.out.println("TryA message");
 
 	}
 
